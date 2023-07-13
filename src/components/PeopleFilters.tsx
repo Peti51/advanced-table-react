0f01776239f0c7f2b660db93a042c0c06ed95dad
Peti51 <galpeter31@gmail.com>
2023-07-13 16:20:43 +0200

    Your commit message

---
+++ src/components/PeopleFilters.tsx
@@ -0,0 +1,112 @@
+import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FilterType } from '../types/enum';
+import { FilterCenturies } from './FilterCenturies';
+import { FilterSex } from './FilterSex';
+import { SearchLink } from './SearchLink';
+
+interface Props {
+  sexFilterHandler: (value: FilterType) => void,
+  setCurrentQuery: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  query: string,
+  handleCenturySelection: (value: number[]) => void,
+  activeCenturies: number[],
+  allCenturySelection: () => void,
+  resetEveryThing: () => void,
+  sexFilter: string | null,
+  setActiveCenturies: (value: number[]) => void,
+  searchParams: URLSearchParams,
+  deleteSearch: () => void,
+  currentUrl: number[],
+}
+
+export const PeopleFilters: React.FC<Props> = ({
+  sexFilterHandler,
+  setCurrentQuery,
+  query,
+  handleCenturySelection,
+  activeCenturies,
+  allCenturySelection,
+  resetEveryThing,
+  sexFilter,
+  setActiveCenturies,
+  searchParams,
+  deleteSearch,
+  currentUrl,
+}) => {
+  useEffect(() => {
+    const activeCenturiesFromParams = searchParams.get('centuries');
+    const centuriesArray = activeCenturiesFromParams
+      ? activeCenturiesFromParams.split(',').map((century) => Number(century))
+      : [];
+
+    setActiveCenturies(centuriesArray);
+  }, []);
+
+  return (
+    <nav className="panel">
+      <p className="panel-heading">Filters</p>
+
+      <FilterSex
+        sexFilter={sexFilter}
+        sexFilterHandler={sexFilterHandler}
+      />
+
+      <div className="panel-block">
+        <p className="control has-icons-left">
+          <input
+            data-cy="NameFilter"
+            type="search"
+            className="input"
+            placeholder="Search"
+            value={query}
+            onChange={(event) => setCurrentQuery(event)}
+          />
+
+          <span className="icon is-left">
+            <i
+              role="button"
+              className="fas fa-search"
+              aria-hidden="true"
+              onClick={deleteSearch}
+            />
+          </span>
+        </p>
+      </div>
+
+      <div className="panel-block">
+        <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
+          <FilterCenturies
+            handleCenturySelection={handleCenturySelection}
+            activeCenturies={activeCenturies}
+            // selectedCenturies={selectedCenturies}
+            currentUrl={currentUrl}
+          />
+
+          <div className="level-right ml-4">
+            <SearchLink
+              data-cy="centuryALL"
+              className={activeCenturies.length === 5
+                ? 'button is-success'
+                : 'button is-outlined'}
+              params={{ centuries: null }}
+              onClick={allCenturySelection}
+            >
+              All
+            </SearchLink>
+          </div>
+        </div>
+      </div>
+
+      <div className="panel-block">
+        <NavLink
+          className="button is-link is-outlined is-fullwidth"
+          to="#/people"
+          onClick={resetEveryThing}
+        >
+          Reset all filters
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,207 @@
+import React, {
+  SetStateAction,
+  Dispatch,
+  useEffect,
+} from 'react';
+import { Person } from '../types';
+import { SortType, PropName } from '../types/enum';
+import { SearchLink } from './SearchLink';
+
+interface Props {
+  people: Person[],
+  handleSort: (value: string) => void,
+  sortOrder: SortType,
+  sortField: string | null,
+  searchParams: URLSearchParams,
+  setSearchParams: Dispatch<SetStateAction<URLSearchParams>>,
+}
+
+export const PeopleTable: React.FC<Props> = ({
+  people,
+  handleSort,
+  sortOrder,
+  sortField,
+  searchParams,
+  setSearchParams,
+}) => {
+  const handleSelection = (slug: string | '') => {
+    if (slug) {
+      searchParams.set('slug', slug);
+
+      setSearchParams(searchParams);
+    }
+  };
+
+  const location = searchParams.get('slug');
+
+  useEffect(() => {
+    const slug = searchParams.get('slug');
+
+    if (slug !== null) {
+      handleSelection(slug);
+    }
+  }, []);
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Name
+              <a
+                href="#/people?sort=name"
+                onClick={() => handleSort(PropName.Name)}
+              >
+                <span className="icon">
+                  {sortOrder === SortType.Asc && sortField === PropName.Name
+                  && <i className="fas fa-sort-up" />}
+                  {sortOrder === SortType.Desc && sortField === PropName.Name
+                   && <i className="fas fa-sort-down" />}
+                  {![SortType.Asc, SortType.Desc].includes(sortOrder)
+                   && <i className="fas fa-sort" />}
+                  {[SortType.Asc, SortType.Desc].includes(sortOrder)
+                   && sortField !== PropName.Name
+                   && (<i className="fas fa-sort" />)}
+                </span>
+              </a>
+            </span>
+          </th>
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Sex
+              <a
+                href="#/people?sort=sex"
+                onClick={() => handleSort(PropName.Sex)}
+              >
+                <span className="icon">
+                  {sortOrder === SortType.Asc && sortField === PropName.Sex
+                  && <i className="fas fa-sort-up" />}
+                  {sortOrder === SortType.Desc && sortField === PropName.Sex
+                   && <i className="fas fa-sort-down" />}
+                  {![SortType.Asc, SortType.Desc].includes(sortOrder)
+                   && <i className="fas fa-sort" />}
+                  {[SortType.Asc, SortType.Desc].includes(sortOrder)
+                   && sortField !== PropName.Sex
+                   && (<i className="fas fa-sort" />)}
+                </span>
+              </a>
+            </span>
+          </th>
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Born
+              <a
+                href="#/people?sort=born&amp;order=desc"
+                onClick={() => handleSort(PropName.Born)}
+              >
+                <span className="icon">
+                  {sortOrder === SortType.Asc && sortField === PropName.Born
+                  && <i className="fas fa-sort-up" />}
+                  {sortOrder === SortType.Desc && sortField === PropName.Born
+                   && <i className="fas fa-sort-down" />}
+                  {![SortType.Asc, SortType.Desc].includes(sortOrder)
+                   && <i className="fas fa-sort" />}
+                  {[SortType.Asc, SortType.Desc].includes(sortOrder)
+                   && sortField !== PropName.Born
+                   && (<i className="fas fa-sort" />)}
+                </span>
+              </a>
+            </span>
+          </th>
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Died
+              <a
+                href="#/people?sort=died"
+                onClick={() => handleSort(PropName.Died)}
+              >
+                <span className="icon">
+                  {sortOrder === SortType.Asc && sortField === PropName.Died
+                  && <i className="fas fa-sort-up" />}
+                  {sortOrder === SortType.Desc && sortField === PropName.Died
+                   && <i className="fas fa-sort-down" />}
+                  {![SortType.Asc, SortType.Desc].includes(sortOrder)
+                   && <i className="fas fa-sort" />}
+                  {[SortType.Asc, SortType.Desc].includes(sortOrder)
+                   && sortField !== PropName.Died
+                    && (<i className="fas fa-sort" />)}
+                </span>
+              </a>
+            </span>
+          </th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+
+        {people.map((person) => {
+          return (
+            <tr
+              key={person.name}
+              className={location === person.slug
+                ? 'has-background-warning'
+                : ''}
+            >
+              <td>
+                <SearchLink
+                  params={{ slug: [String(person.slug)] }}
+                  className={person.sex === 'f'
+                    ? ('has-text-danger')
+                    : ''}
+                  role="button"
+                  onClick={() => handleSelection(person.slug)}
+                >
+                  {person.name}
+                </SearchLink>
+              </td>
+              <td>{person.sex}</td>
+              <td>{person.born}</td>
+              <td>{person.died}</td>
+              <td>
+                {person.mother
+                  ? (
+                    <SearchLink
+                      params={{ slug: [String(person.mother?.slug)] }}
+                      className="has-text-danger"
+                      onClick={() => {
+                        handleSelection(person.mother?.slug || '');
+                      }}
+                    >
+                      {person.mother?.name}
+                    </SearchLink>
+                  ) : (
+                    <p>
+                      {person.motherName}
+                    </p>
+                  )}
+              </td>
+              <td>
+                {person.father
+                  ? (
+                    <SearchLink
+                      params={{ slug: [String(person.father?.slug)] }}
+                      onClick={() => {
+                        handleSelection(person.father?.slug || '');
+                      }}
+                    >
+                      {person.father.name}
+                    </SearchLink>
+                  ) : (
+                    <p>
+                      {person.fatherName}
+                    </p>
+                  )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
